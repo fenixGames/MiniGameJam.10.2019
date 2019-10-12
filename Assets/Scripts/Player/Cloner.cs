f21e94a095a26264c718326ddc66a3a31f898934
lara <larafercue@gmail.com>
2019-10-12 17:02:07 +0200

    Add a clone script to place towers on the game

---
+++ Assets/Scripts/Player/Cloner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject towerPrefab;
+
+    [SerializeField]
+    private float cloningTimeout = 10.0f;
+
+    private float timer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) && timer > cloningTimeout)
+        {
+            GameObject tower = Instantiate(towerPrefab);
+            tower.transform.position = transform.position;
+            timer = 0.0f;
+        }
+        if (timer < cloningTimeout)
+            timer += Time.deltaTime;
+    }
+}
