0ffad89bc8603dd3f93465253045fa24d9f7b7d7
Simon <fate1990@googlemail.com>
2019-10-12 20:41:51 +0200

    map 3

---
+++ Assets/CamMovement2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement2 : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + offset;
+
+    }
+}
--- Assets/Scripts/WorldMouseMovement.cs
+++ Assets/Scripts/WorldMouseMovement.cs
@@ -6,7 +6,9 @@ public class WorldMouseMovement : MonoBehaviour
 {
     [SerializeField]
     private float trackSpeed = 1.0f;
-
+    public Vector3 offset;
+    Vector3 moveVector;
+    public Transform reference;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,17 @@ public class WorldMouseMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveVector = Vector3.zero;
+        //Vector3 moveVector = Vector3.zero;
 
-        moveVector.x = Input.GetAxis("Mouse X");
-        moveVector.z = Input.GetAxis("Mouse Y");
+        moveVector.x += Input.GetAxis("Mouse X");
+        moveVector.z += Input.GetAxis("Mouse Y");
 
-        transform.position += trackSpeed * moveVector;
+        Vector3 localPos = transform.localPosition;
+        //localPos.y = 0;
+
+        //transform.localPosition = localPos;
+        transform.position =  reference.transform.position + trackSpeed * moveVector;
+
+
     }
 }
