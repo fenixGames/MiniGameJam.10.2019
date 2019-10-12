85d6174d318d719410e426a5f32fff82698c560a
Robert Klares <klaro115@hotmail.com>
2019-10-12 15:14:33 +0200

    Added damage model

---
+++ Assets/Scripts/DamageModel/DamageType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+[Flags]
+public enum DamageType
+{
+    Normal		= 0x01,		// Standard physical damage type.
+
+	// TODO: Add more damage types once they've been decided upon by game design folks.
+
+	None		= 0
+}
