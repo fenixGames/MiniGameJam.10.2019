6fcbfa764658aad55238392d2d87867f2a33e9a2
Robert Klares <klaro115@hotmail.com>
2019-10-12 20:49:32 +0200

    Added new projectile effects and prefabs.

--- Assets/Scripts/DamageModel/DamageType.cs
+++ Assets/Scripts/DamageModel/DamageType.cs
@@ -6,5 +6,7 @@ public enum DamageType
 {
     Normal		= 0x01,		// Standard physical damage type.
 
+	Lightning	= 0x02,
+
 	// TODO: Add more damage types once they've been decided upon by game design folks.
 }
