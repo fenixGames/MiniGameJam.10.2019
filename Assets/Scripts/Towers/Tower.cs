8091145e3c72bcbf7adf7b8c4ec86c4fa9b14fc3
Robert Klares <klaro115@hotmail.com>
2019-10-12 20:14:14 +0200

    Fixed even more typos in tower.

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -107,7 +107,15 @@ public class Tower : MonoBehaviour
 		}
 
 		// Set the selected object as our new target:
-		if (target != bestTargetGO) targetAgent = target.GetComponent<NavMeshAgent>();
+		GameObject previousTarget = target;
+		if (target != null)
+		{
+			if (previousTarget != target) targetAgent = bestTargetGO.GetComponent<NavMeshAgent>();
+		}
+		else
+		{
+			targetAgent = null;
+		}
 		target = bestTargetGO;
 	}
 
