624df4f3e5ebdf4dd52d989b5605ea87e3fbffb3
Robert Klares <klaro115@hotmail.com>
2019-10-12 19:15:40 +0200

    Towers can now be set to use either yaw-only or free rotation when aiming.

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -1,20 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+	public enum TurnMode
+	{
+		AllAxis,
+		YawOnly
+	}
+
+	public TurnMode turnMode = TurnMode.AllAxis;
+
 	public string targetTag = "Enemy";
 	public float attackRange = 3.0f;			// The maximum attack range before engaging a target.
 	public float maxTurnRate = 360.0f;      	// max. rotation change in degrees per second.
 	public float minFiringAngle = 5.0f;			// min. angle towards target within which we may open fire.
 	public float scanInterval = 0.1f;       	// Every how many seconds to scan for targets to shoot at.
-	public float projectileSpeed = 5.0f;		// Starting speed of projectiles fired at the target, in meters per second.
+	public float projectileSpeed = 5.0f;        // Starting speed of projectiles fired at the target, in meters per second.
+	public float fireInterval = 0.5f;			// min. time interval between consecutive shots fired, in seconds.
 
 	private GameObject target = null;
 	private float currentAngle = 0.0f;
 	private float targetAngle = 0.0f;
 	private float lastScanTime = -1.0f;
+	private float lastFireTime = -1.0f;
 
 	public Projectile projectilePrefab = null;
 	public Damage damage = Damage.Standard; 	// The damage output per shot.
@@ -31,8 +39,9 @@ public class Tower : MonoBehaviour
 		// Scan and select targets every so many seconds:
 		if(Time.time > lastScanTime + scanInterval)
 		{
-			DetectTargets();
 			lastScanTime = Time.time;
+
+			DetectTargets();
 		}
 
 		// Return if no target is available:
@@ -42,9 +51,11 @@ public class Tower : MonoBehaviour
 		RotateTowardsTarget();
 
 		// Fire at the target:
-		if (currentAngle < minFiringAngle)
+		if (currentAngle < minFiringAngle && Time.time > lastFireTime + fireInterval)
 		{
-			Vector3 velocity = transform.right * projectileSpeed;
+			lastFireTime = Time.time;
+
+			Vector3 velocity = transform.forward * projectileSpeed;
 			CombatSystem.Fire(transform.position, velocity, projectilePrefab, damage);
 		}
 	}
@@ -58,8 +69,6 @@ public class Tower : MonoBehaviour
 			target = null;
 			return;
 		}
-
-		//Debug.Log($"Found targets: {targets.Length}");
 
 		// Select the closest enemy to self:
 		float attackRangeSq = attackRange * attackRange;
@@ -81,16 +90,28 @@ public class Tower : MonoBehaviour
 
 	private void RotateTowardsTarget()
 	{
-		// Calculate target angle towards current target:
 		Vector3 targetDir = (target.transform.position - transform.position).normalized;
-		targetAngle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg + 90;
-
-		// Rotate towards target angle:
-		float previousAngle = currentAngle;
 		float maxAngleDiff = maxTurnRate * Time.deltaTime;
-		currentAngle = Mathf.MoveTowardsAngle(previousAngle, targetAngle, maxAngleDiff);
 
-		// Apply rotation to transform:
-		transform.eulerAngles = new Vector3(0, currentAngle, 0);
+		if (turnMode == TurnMode.AllAxis)
+		{
+			// Calculate and apply rotations along all axis:
+			Quaternion targetRotation = Quaternion.LookRotation(targetDir);
+			Quaternion prevRotation = transform.rotation;
+			transform.rotation = Quaternion.RotateTowards(prevRotation, targetRotation, maxAngleDiff);
+			currentAngle = Vector3.Angle(transform.forward, targetDir);
+		}
+		else if (turnMode == TurnMode.YawOnly)
+		{
+			// Calculate target angle towards current target:
+			targetAngle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg + 90;
+
+			// Rotate towards target angle:
+			float previousAngle = currentAngle;
+			currentAngle = Mathf.MoveTowardsAngle(previousAngle, targetAngle, maxAngleDiff);
+
+			// Apply rotation to transform:
+			transform.eulerAngles = new Vector3(0, currentAngle, 0);
+		}
 	}
 }
